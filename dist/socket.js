f147d5498d26b1080fb2d2a8890458e4f82b3d5b
Mayank <may.gh96@gmail.com>
2023-09-10 21:52:21 +0530

    to add a new package

--- dist/socket.js
+++ dist/socket.js
@@ -4,22 +4,23 @@ exports.messageReceived = exports.initializeSocket = void 0;
 const socket_io_1 = require("socket.io");
 let io;
 function initializeSocket() {
-    io = new socket_io_1.Server(5000, {
-        cors: {
-            origin: "*",
-        },
+  io = new socket_io_1.Server(5000, {
+    cors: {
+      origin: "*",
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log("socket connection establised: ");
+    socket.on("customEvent", (data) => {
+      console.log("Received data from client:", data);
+      // Send a message to the client
+      io.emit("serverMessage", "This is a message from the server");
     });
-    io.on("connection", (socket) => {
-        console.log("socket connection establised: ");
-        socket.on("customEvent", (data) => {
-            console.log("Received data from client:", data);
-            // Send a message to the client
-            io.emit("serverMessage", "This is a message from the server");
-        });
-    });
+  });
 }
 exports.initializeSocket = initializeSocket;
 function messageReceived(message) {
-    io.emit("send_message", message);
+  console.log(message);
+  io.emit("send_message", message);
 }
 exports.messageReceived = messageReceived;
